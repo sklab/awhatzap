1b116838d0ab9dfd8bdb1b18011a2edf2bbd06bc
sklab <takamineu@gmail.com>
2023-04-28 16:02:13 +0900

    use Workbox

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,44 @@
-self.addEventListener('install', (event) => {
-  console.log('service worker install ...');
-  
-  const cacheKey = 'SiteCache_v1';
+importScripts('workbox-sw.js');
 
-  event.waitUntil(caches.open(cacheKey).then((cache) => {
-    return cache.addAll([
-    ]);
-  }));
+const CACHE = "awhatzap-page";
+
+const offlineFallbackPage = "index.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('activate', (event) => {
-  console.info('activate', event);
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
